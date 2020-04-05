447ac9e9e010da9d333ea2d4c0b16cbd6e959f5d
haraj2000 <haja.senabo@mail.com>
2020-04-05 18:31:10 +0100

    controller khdm

--- src/main/java/org/mirrane/App.java
+++ src/main/java/org/mirrane/App.java
@@ -24,7 +24,8 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        scene = new Scene(loadFXML("secondary"));
+
+        scene = new Scene(loadFXML("AddTypeAppointement"));
         stage.setScene(scene);
         stage.show();
 
@@ -61,7 +62,7 @@ public class App extends Application {
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource( fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
